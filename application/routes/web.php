4726aff0e5f2211527434814f81aec1a6324ebc6
DaveCybr <davidrngg4@gmail.com>
2025-09-19 19:05:43 +0700

    .

--- application/routes/web.php
+++ application/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
+        'name' => 'Parental Control API',
         'status' => 'success',
         'message' => 'Welcome to Parental Control API',
         'version' => '1.0.0'
